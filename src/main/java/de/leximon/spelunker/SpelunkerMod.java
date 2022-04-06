6fcf8db59400ae0150289a5944ad12ecb439c789
Lex305 <lex305@web.de>
2022-04-06 20:45:46 +0200

    fix amethyst dust drops with silk touch

--- src/main/java/de/leximon/spelunker/SpelunkerMod.java
+++ src/main/java/de/leximon/spelunker/SpelunkerMod.java
@@ -6,17 +6,25 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
+import net.minecraft.loot.condition.LootCondition;
+import net.minecraft.loot.condition.LootConditionType;
+import net.minecraft.loot.condition.LootConditionTypes;
+import net.minecraft.loot.condition.MatchToolLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetPotionLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.Potions;
+import net.minecraft.predicate.NumberRange;
+import net.minecraft.predicate.item.EnchantmentPredicate;
+import net.minecraft.predicate.item.ItemPredicate;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
@@ -80,6 +88,11 @@ public class SpelunkerMod implements ModInitializer {
 			if(AMETHYST_CLUSTER_LOOT_TABLE.equals(id)) {
 				table.pool(FabricLootPoolBuilder.builder()
 						.rolls(ConstantLootNumberProvider.create(1))
+								.withCondition(MatchToolLootCondition.builder(ItemPredicate.Builder.create()
+												.enchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, NumberRange.IntRange.ANY)))
+										.invert()
+										.build()
+								)
 						.with(ItemEntry.builder(AMETHYST_DUST)
 								.weight(1)
 						)
