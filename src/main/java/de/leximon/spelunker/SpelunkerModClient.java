c4ba663a19cf343e3d77c63bed49582c3b88efd9
Leximon <lex305@web.de>
2023-02-01 16:29:26 +0100

    fix spelunker effect lagging behind (especially on low frame rates)

--- src/main/java/de/leximon/spelunker/SpelunkerModClient.java
+++ src/main/java/de/leximon/spelunker/SpelunkerModClient.java
@@ -8,30 +8,20 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.minecraft.client.MinecraftClient;
 
 public class SpelunkerModClient implements ClientModInitializer {
 
     public static SpelunkerEffectRenderer spelunkerEffectRenderer = new SpelunkerEffectRenderer();
-    public static boolean isAlreadyRenderingOutline = false;
 
     @Override
     public void onInitializeClient() {
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> SpelunkerConfig.initBlockHighlightConfig());
 
         // register renderer
-        WorldRenderEvents.LAST.register(context -> {
+        WorldRenderEvents.AFTER_ENTITIES.register(context -> {
             WorldRendererAccessor worldRenderer = (WorldRendererAccessor) context.worldRenderer();
-            MinecraftClient client = MinecraftClient.getInstance();
-
-            if (spelunkerEffectRenderer.isActive()) {
-                if (!isAlreadyRenderingOutline) { // prevent the outline shader from being rendered twice due glowing entities
-                    worldRenderer.getEntityOutlinePostProcessor().render(context.tickDelta());
-                    client.getFramebuffer().beginWrite(false);
-                }
+            if (spelunkerEffectRenderer.isActive())
                 spelunkerEffectRenderer.render(context.matrixStack(), context.camera(), worldRenderer.getBufferBuilders().getOutlineVertexConsumers());
-            }
-            isAlreadyRenderingOutline = false;
         });
 
         ClientPlayNetworking.registerGlobalReceiver(SpelunkerMod.PACKET_ORE_CHUNKS, (client, handler, buf, sender) -> SpelunkerEffectManager.readPacket(spelunkerEffectRenderer, buf));
--- src/main/java/de/leximon/spelunker/mixin/client/WorldRendererMixin.java
+++ src/main/java/de/leximon/spelunker/mixin/client/WorldRendererMixin.java
@@ -1,22 +1,17 @@
 package de.leximon.spelunker.mixin.client;
 
 import de.leximon.spelunker.SpelunkerModClient;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
 
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/PostEffectProcessor;render(F)V"))
-    private void inject(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
-        SpelunkerModClient.isAlreadyRenderingOutline = true;
+    @SuppressWarnings("InvalidInjectorMethodSignature") // Minecraft Development plugin is crazy again
+    @ModifyVariable(method = "render", at = @At(value = "CONSTANT", args = "stringValue=blockentities", ordinal = 0), ordinal = 4)
+    private boolean modify(boolean value) {
+        return value || SpelunkerModClient.spelunkerEffectRenderer.isActive();
     }
 }
